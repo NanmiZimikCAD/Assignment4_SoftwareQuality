a3841f76607e57596fe1770e8c9cd07b30d31606
Nanmi Zimik <nzimik5599@conestogac.on.ca>
2025-04-10 19:00:00 -0400

    set up NUnit and Selenium WebDriver project with ChromeDriver configuration

---
+++ UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+
+namespace InsuranceTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+        }
+    }
+}
